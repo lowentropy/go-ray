d7a32d6a7861fba7f5f333278da19d0d635e2a39
Nathan Matthews <lowentropy@gmail.com>
2013-05-15 11:33:13 -0500

    Fun scene

--- cornell.go
+++ cornell.go
@@ -6,10 +6,14 @@ func cornell(w, h int) *scene {
 
 	// glass &sphere
 	// this is the original location
-	scene.Add(&body{&sphere{vec3{1, 0, -2}, 0.5}, &glass{color{0.8, 1.0, 0.8}, 1.5, 0.1}})
+	scene.Add(&body{&sphere{vec3{1, 0, -2}, 0.5}, &glass{color{0.8, 0.8, 1.0}, 1.5, 0.1}})
+
+	// these are for fun
+	scene.Add(&body{&sphere{vec3{-1, 0, -2}, 0.5}, &glass{color{1.0, 0.8, 0.8}, 1.5, 0.1}})
+	scene.Add(&body{&sphere{vec3{0, 0, -2}, 0.5}, &glass{color{0.8, 1.0, 0.8}, 1.5, 0.1}})
 
 	// chrome &sphere
-	scene.Add(&body{&sphere{vec3{-1.1, 0, -2.8}, 0.5}, &chrome{color{0.8, 0.8, 0.8}}})
+	// scene.Add(&body{&sphere{vec3{-1.1, 0, -2.8}, 0.5}, &chrome{color{0.8, 0.8, 0.8}}})
 
 	// floor
 	scene.Add(&body{&sphere{vec3{0, -10e6, -3.5}, 10e6 - 0.5}, &dull{color{0.9, 0.9, 0.9}}})
